200129152f65b687ace85c2f6fcc3d904aea254f
sorivevega <sorivevega@educacion.navarra.es>
2024-10-25 18:18:12 +0200

    Finalizado Ejercicio 13

---
+++ Ejercicio13.py
@@ -0,0 +1,5 @@
+#Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
+while True:
+    info = input('Escribe lo que quieras: ')
+    if info == 'salir':
+        break
